e56da7abd4e8205f6ff57f913105ede11406b6e5
Pete Lumbis <plumbis@cumulusnetworks.com>
2017-09-06 08:46:41 -0700

    adds mgmt vrf ztp script

--- helper_scripts/oob-mgmt-server-netq-setup.sh
+++ helper_scripts/oob-mgmt-server-netq-setup.sh
@@ -2,3 +2,34 @@
 sudo su - cumulus -c '\
 git clone https://github.com/cumulusnetworks/netqdemo-1.0 netqdemo; \
 '
+
+cat << EOT > /var/www/ztp.sh
+#!/bin/bash
+function error() {
+  echo -e "\e[0;33mERROR: The Zero Touch Provisioning script failed while running the command $BASH_COMMAND at line $BASH_LINENO.\e[0m" >&2
+  exit 1
+}
+
+cat << DONE > /etc/network/interfaces
+ # The loopback network interface
+ auto lo
+ iface lo inet loopback
+
+ # The primary network interface
+ auto eth0
+ iface eth0 inet dhcp
+    vrf mgmt
+
+auto mgmt
+iface mgmt
+    address 127.0.0.1/8
+    vrf-table auto
+DONE
+
+ifreload -a
+
+# CUMULUS-AUTOPROVISIONING
+exit 0
+EOT
+
+chmod +r /var/www/ztp.sh
