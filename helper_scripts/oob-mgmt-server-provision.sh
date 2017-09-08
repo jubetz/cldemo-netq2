e00050ad05a0dca871397220d185cfa5c19df6d8
Pete Lumbis <plumbis@cumulusnetworks.com>
2017-09-08 14:56:49 -0700

    adds jessie security updates

--- helper_scripts/oob-mgmt-server-provision.sh
+++ helper_scripts/oob-mgmt-server-provision.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo sh -c 'echo "deb http://httpredir.debian.org/debian jessie main" > /etc/apt/sources.list.d/jessie.list'
 sudo sh -c 'echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list.d/jessie.list'
+sudo sh -c 'echo "deb http://security.debian.org/ jessie/updates main" >> /etc/apt/sources.list.d/jessie.list'
 sudo sh -c 'echo "deb http://repo3.cumulusnetworks.com/repo Jessie-supplemental upstream" > /etc/apt/sources.list.d/jessie_cl.list'
 sudo apt-get update
 sudo apt-get install -yq git python-netaddr sshpass
