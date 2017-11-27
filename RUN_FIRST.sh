383cdc3b94801f0f617d27584ce760a830d64e49
Pete Lumbis <plumbis@cumulusnetworks.com>
2017-11-27 11:15:31 -0800

    add ansible config

--- RUN_FIRST.sh
+++ RUN_FIRST.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 wget -P /etc/ansible/ https://raw.githubusercontent.com/CumulusNetworks/cldemo-netq/citc/hosts
+wget -P /etc/ansible/ https://raw.githubusercontent.com/CumulusNetworks/cldemo-netq/citc/ansible.cfg
 
 ansible network -a "net add vrf mgmt"
 ansible network -a "net commit"
