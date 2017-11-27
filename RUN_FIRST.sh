c588bb27effb2bd96df65695de92b2ee439c636d
Pete Lumbis <plumbis@cumulusnetworks.com>
2017-11-27 11:19:22 -0800

    has cumulus user run ansible commands

--- RUN_FIRST.sh
+++ RUN_FIRST.sh
@@ -3,8 +3,8 @@
 wget -P /etc/ansible/ https://raw.githubusercontent.com/CumulusNetworks/cldemo-netq/citc/hosts
 wget -P /etc/ansible/ https://raw.githubusercontent.com/CumulusNetworks/cldemo-netq/citc/ansible.cfg
 
-ansible network -a "net add vrf mgmt"
-ansible network -a "net commit"
+su cumulus ansible network -a "net add vrf mgmt"
+su cumulus ansible network -a "net commit"
 
 git init
 git clone -b citc https://github.com/CumulusNetworks/cldemo-netq.git
